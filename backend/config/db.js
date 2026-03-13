d314aa1449ef5253077b251df3c920abcdec9b23
sanjay-rs-3355 <sanjay33556y@gmail.com>
2026-03-13 10:29:40 +0530

    Enable SSL for database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,7 +10,10 @@ const db = mysql.createPool({
     port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = db;
